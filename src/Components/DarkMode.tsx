59f93285159db00381fdf3f776d2ce3aca12092b
Francine <franmelopepe2@gmail.com>
2023-06-08 18:15:27 +0200

    darkmode component finished

--- src/Components/DarkMode.tsx
+++ src/Components/DarkMode.tsx
@@ -1,18 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Moon from "../Icons/Moon";
 import DarkMoon from "../Icons/DarkMoon";
 import "../Styles/styles.sass";
 
 function DarkMode() {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
   };
 
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div>
-      <button className="button-darkmode" onClick={toggleDarkMode}>
-        {darkMode ? <DarkMoon /> : <Moon />} Dark Mode
+    <div className={`DarkMode ${theme}`}>
+      <button className="button-darkmode" onClick={toggleTheme}>
+        {theme ? <Moon /> : <DarkMoon />} Dark Mode
       </button>
     </div>
   );
